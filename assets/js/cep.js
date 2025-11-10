40d5367eabb23db6d3ccbed6a84f7ca4836b206a
ArthurMolinaM <gitarthurmolina@gmail.com>
2025-11-10 10:53:36 -0300

    adicionando coisas necessarias

--- assets/js/cep.js
+++ assets/js/cep.js
@@ -61,3 +61,43 @@ cepInput.addEventListener('input',(e) => {
     e.target.value = valorDigitado;
 })
 
+// Função para salvar no Storage
+// recuperar o form para salvar os dados
+form.addEventListener('submit', (e) =>{
+  // cancelar o evento padrão
+  e.preventDefault();
+
+  // recuperar o que foi digitado no formulário e passar para as variaveis
+   const nome = document.querySelector('#nome').value;
+   const cep = document.querySelector('#cep').value;
+   const logradouro = document.querySelector('#logradouro').value;
+   const numero = document.querySelector('#numero').value;
+   const complemento = document.querySelector('#complemento').value;
+   const bairro = document.querySelector('#bairro').value;
+   const cidade = document.querySelector('#cidade').value;
+   const estado = document.querySelector('#estado').value;
+
+  //  verificar se existe algum campo vazio
+  // vamos comparar as variaveis
+  if(!nome || !cep || !logradouro || !numero || !bairro || !cidade || !estado){
+    alert('Por favor, preencha todos os campos!')
+  };
+
+  // Gerar o objeto para a gravação
+  const usuario = { nome, cep, logradouro, numero, complemento, bairro, cidade, estado};
+
+  // verificar se existe usuario objeto ja gravado, não existindo ele retorna um array vazio
+  let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+  // inserir o objeto usuario no fim do retorno
+  usuarios.push(usuario);
+
+  // grava no objeto na chave usuarios
+  localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+  alert('Dados gravados com sucesso!')
+
+  // limpar os dados do formulario
+  form.querySelectorAll('input').forEach(input => input.value = '')
+   
+})
+
